32172e66a332a073a0817e13f52ed737ab457036
Sean McPheeters <mcpheeterssean@gmail.com>
2018-08-25 16:31:26 -0700

    Updated Filter Gallery

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'images'));
+
 
 //Routes to all pages
 app.get('/', function(req, res){
